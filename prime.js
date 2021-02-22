d6d03c046da155055e3e82d331e3c68d024a5c12
Jhonatan Tomimatsu <jhonatan.tft@gmail.com>
2021-02-22 00:56:13 -0300

    add: prime algorithm

---
+++ prime.js
@@ -0,0 +1,19 @@
+/**
+ * O(n)
+ * @param {*} num 
+ */
+const isPrime = num => {
+  for (let i = 2; i < num; i++)
+    if (num % i === 0) return false;
+  return num > 1;
+}
+
+/**
+ * O(sqrt(n))
+ * @param {*} num 
+ */
+const isPrime = num => {
+  for (let i = 2, s = Math.sqrt(num); i <= s; i++)
+    if (num % i === 0) return false;
+  return num > 1;
+}
